865721057dad667e54a1216817770c76996156c4
harikiranrommala <h.rommala@northeastern.edu>
2025-01-22 22:28:55 -0500

    Webpage building (#3)
    * Webpage building
    
    * [Fix][Remove package-lock
    
    * [Fix][Validation]
    
    * [Fix][Validation]
    
    * [Fix][Validation]
    
    * [Fix][Validation]
    
    * [Fix][Validation]
    
    * [Fix][Code]Structure update
    
    * [Fix][Code]Structure update
    
    * [Fix][Code]Structure update
    
    * [Fix][Slice]Return setLoading
    
    * [Fix][Slice]Return setLoading
    
    * [Fix][Slice]Return setLoading
    
    ---------
    
    Co-authored-by: Hari Kiran <hari@Haris-Laptop.local>
    Co-authored-by: MarSHub <madlsh@icloud.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { Container } from "@mui/material";
+import { Box, Typography, Button, Container } from "@mui/material";
 import { useAppDispatch } from "hooks/useAppDispatch";
 import { useAppSelector } from "hooks/useAppSelector";
 import NeuroJsonGraph from "modules/universe/NeuroJsonGraph";
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { fetchRegistry } from "redux/neurojson/neurojson.action";
 import { NeurojsonSelector } from "redux/neurojson/neurojson.selector";
 
@@ -12,9 +13,34 @@ const Home: React.FC = () => {
 
 	useEffect(() => {
 		dispatch(fetchRegistry());
+		console.log(registry);
 	}, [dispatch]);
 	return (
-		<Container>{registry && <NeuroJsonGraph registry={registry} />}</Container>
+		<Container>
+			{registry && <NeuroJsonGraph registry={registry} />}
+
+			<Box sx={{ padding: 4, maxHeight: "100%", maxWidth: "30%" }}>
+				{/* Header Section */}
+				<Typography variant="h3" gutterBottom>
+					Welcome to NeuroJSON IO
+				</Typography>
+				<Typography variant="body1">
+					Manage and explore your CouchDB databases and datasets effortlessly.
+				</Typography>
+
+				{/* Navigation to Database Page */}
+				<Box mt={4}>
+					<Button
+						variant="contained"
+						color="primary"
+						component={Link}
+						to="/databases"
+					>
+						View Databases
+					</Button>
+				</Box>
+			</Box>
+		</Container>
 	);
 };
 
