17f0ee9fbe35ac26dc444a3c2a57a6bed32f9889
Hari Kiran <hari@Haris-Laptop.local>
2025-03-12 13:37:27 -0400

    Fix: Adjust Webpack config to handle dynamic dependencies

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,13 @@ module.exports = {
           ...webpackConfig.resolve.fallback,
           path: require.resolve("path-browserify"),
         };
+
+        webpackConfig.module.rules.push({
+          test: /\.js$/,
+          parser: {
+            requireEnsure: false, // Prevents Webpack from complaining about dynamic requires
+          },
+        });
         return webpackConfig;
       },
     },
