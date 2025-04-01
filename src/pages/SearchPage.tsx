1fec4104648554fb5ed19ee1df5ce9c5c0035875
elainefan331 <elainefan331@gmail.com>
2025-04-01 16:37:24 -0400

    Update navigation to link search to /search route

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const SearchPage: React.FC = () => {
+  return <Typography variant="h1">Search Page</Typography>;
+};
+
+export default SearchPage;
