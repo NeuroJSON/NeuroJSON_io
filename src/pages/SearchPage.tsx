ff7876404745286b39273d52b010abf5c5f5fac1
elainefan331 <elainefan331@gmail.com>
2025-04-04 15:10:30 -0400

    feat: integrate metadata search form with Redux for state management

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,81 +2,25 @@ import { schema } from "./searchformSchema";
 import { Typography, Container, Box } from "@mui/material";
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
+import { Colors } from "design/theme";
+import { useAppDispatch } from "hooks/useAppDispatch";
+import { useAppSelector } from "hooks/useAppSelector";
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { fetchMetadataSearchResults } from "redux/neurojson/neurojson.action";
+import { RootState } from "redux/store";
 import RoutesEnum from "types/routes.enum";
 
-// helper function to build query string
-const buildQueryString = (formData: any): string => {
-  const map: Record<string, string> = {
-    keyword: "keyword",
-    age_min: "agemin",
-    age_max: "agemax",
-    task_min: "taskmin",
-    task_max: "taskmax",
-    run_min: "runmin",
-    run_max: "runmax",
-    sess_min: "sessmin",
-    sess_max: "sessmax",
-    modality: "modality",
-    run_name: "run",
-    type_name: "type",
-    session_name: "session",
-    task_name: "task",
-    limit: "limit",
-    skip: "skip",
-    count: "count",
-    unique: "unique",
-    gender: "gender",
-    database: "dbname",
-    dataset: "dsname",
-    subject: "subname",
-  };
-
-  const params = new URLSearchParams();
-  Object.keys(formData).forEach((key) => {
-    let val = formData[key];
-    if (val === "" || val === "any" || val === undefined || val === null)
-      return;
-
-    const queryKey = map[key];
-    if (!queryKey) return;
-
-    if (key.startsWith("age")) {
-      params.append(queryKey, String(Math.floor(val * 100)).padStart(5, "0"));
-    } else if (key === "gender") {
-      params.append(queryKey, val[0]);
-    } else if (key === "modality") {
-      params.append(queryKey, val.replace(/.*\(/, "").replace(/\).*/, ""));
-    } else {
-      params.append(queryKey, val.toString());
-    }
-  });
-
-  return `?${params.toString()}`;
-};
-
 const SearchPage: React.FC = () => {
-  const [result, setResult] = useState<any>(null);
+  const dispatch = useAppDispatch();
   const [hasSearched, setHasSearched] = useState(false);
+  const searchResults = useAppSelector(
+    (state: RootState) => state.neurojson.searchResults
+  );
 
-  //   const handleSubmit = ({ formData }: any) => {
-  //     console.log("submitted search query:", formData);
-  //   };
-  const handleSubmit = async ({ formData }: any) => {
-    console.log("submitted search query:", formData);
-    const query = buildQueryString(formData);
-    const url = `https://cors.redoc.ly/https://neurojson.org/io/search.cgi${query}`;
-    // console.log("url", url);
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setResult(data);
-      console.log(data);
-    } catch (err) {
-      console.error("Failed to fetch data:", err);
-    }
+  const handleSubmit = ({ formData }: any) => {
+    dispatch(fetchMetadataSearchResults(formData));
     setHasSearched(true);
   };
 
@@ -97,24 +41,16 @@ const SearchPage: React.FC = () => {
           liveValidate
         />
 
-        {/* {result && (
+        {hasSearched && searchResults && (
           <Box mt={4}>
-            <Typography variant="h6">Datasets Found</Typography>
-            <pre style={{ background: "#f5f5f5", padding: "1rem" }}>
-              {JSON.stringify(result, null, 2)}
-            </pre>
-          </Box>
-        )} */}
-        {hasSearched && (
-          <Box mt={4}>
-            {Array.isArray(result) ? (
-              result.length > 0 ? (
+            {Array.isArray(searchResults) ? (
+              searchResults.length > 0 ? (
                 <>
                   <Typography variant="h6">
-                    {`Found ${result.length} Datasets`}
+                    {`Found ${searchResults.length} Datasets`}
                   </Typography>
                   <ul>
-                    {result.map((item, idx) => {
+                    {searchResults.map((item, idx) => {
                       const label = `${item.dbname}/${item.dsname}`;
                       const link = `${RoutesEnum.DATABASES}/${item.dbname}/${item.dsname}`;
 
@@ -122,7 +58,10 @@ const SearchPage: React.FC = () => {
                         <Box key={idx} mb={1}>
                           <Link
                             to={link}
-                            style={{ textDecoration: "none", color: "#1976d2" }}
+                            style={{
+                              textDecoration: "none",
+                              color: Colors.blue,
+                            }}
                           >
                             {label}
                           </Link>
@@ -138,7 +77,7 @@ const SearchPage: React.FC = () => {
               )
             ) : (
               <Typography color="error">
-                {result?.msg === "empty output"
+                {searchResults?.msg === "empty output"
                   ? "No results found based on your criteria. Please adjust the filters and try again."
                   : "Something went wrong. Please try again later."}
               </Typography>
