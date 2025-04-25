b66e020316f28967d94b75d4b6fd087682ddac93
elainefan331 <elainefan331@gmail.com>
2025-04-25 12:15:52 -0400

    refactor: extract section2 into dedicated component; refs #50

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,10 @@
-import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
-import {
-  Box,
-  Typography,
-  Button,
-  Container,
-  Grid,
-  CircularProgress,
-} from "@mui/material";
-import SearchBar from "components/HomePageComponents/SearchBar";
+import { Box, Typography, Button, Container } from "@mui/material";
+import Section1 from "components/HomePageComponents/Section1";
+import Section2 from "components/HomePageComponents/Section2";
 import NodeInfoPanel from "components/NodeInfoPanel";
-import FilterMenu from "components/NodesFilter/FilterMenu";
 import { Colors } from "design/theme";
 import { useAppDispatch } from "hooks/useAppDispatch";
 import { useAppSelector } from "hooks/useAppSelector";
-import NeuroJsonGraph from "modules/universe/NeuroJsonGraph";
 import { NodeObject } from "modules/universe/NeuroJsonGraph";
 import React, { useEffect, useState, useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
@@ -69,233 +60,22 @@ const Home: React.FC = () => {
         overflow: "hidden",
         position: "relative",
         minHeight: "500px",
-        // display: "flex",
-        // flexDirection: "column",
         // minHeight: "100vh",
       }}
     >
-      {/* <Box // add box for group StatisticsBanner and 3d-graph
-        sx={{
-          flex: "2",
-          width: "100%",
-          position: "relative",
-          overflow: "hidden",
-          // minHeight: "100vh",
-          // minHeight: "90%",
-          // maxHeight: "80vh",
-        }}
-      > */}
       {/* section 1 */}
-      <SearchBar />
+      <Section1 />
 
       {/* section 2 */}
-      <Box // white background
-        sx={{
-          zIndex: "2",
-          position: "relative",
-          width: "100%",
-          backgroundColor: Colors.white,
-          padding: "5rem 7rem",
-
-          // overflow: "hidden",
-          // backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3CradialGradient id='a' gradientUnits='objectBoundingBox'%3E%3Cstop offset='0' stop-color='%23000000'/%3E%3Cstop offset='1' stop-color='%235865F2'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='750' x2='1550' y2='750'%3E%3Cstop offset='0' stop-color='%232c3379'/%3E%3Cstop offset='1' stop-color='%235865F2'/%3E%3C/linearGradient%3E%3Cpath id='s' fill='url(%23b)' d='M1549.2 51.6c-5.4 99.1-20.2 197.6-44.2 293.6c-24.1 96-57.4 189.4-99.3 278.6c-41.9 89.2-92.4 174.1-150.3 253.3c-58 79.2-123.4 152.6-195.1 219c-71.7 66.4-149.6 125.8-232.2 177.2c-82.7 51.4-170.1 94.7-260.7 129.1c-90.6 34.4-184.4 60-279.5 76.3C192.6 1495 96.1 1502 0 1500c96.1-2.1 191.8-13.3 285.4-33.6c93.6-20.2 185-49.5 272.5-87.2c87.6-37.7 171.3-83.8 249.6-137.3c78.4-53.5 151.5-114.5 217.9-181.7c66.5-67.2 126.4-140.7 178.6-218.9c52.3-78.3 96.9-161.4 133-247.9c36.1-86.5 63.8-176.2 82.6-267.6c18.8-91.4 28.6-184.4 29.6-277.4c0.3-27.6 23.2-48.7 50.8-48.4s49.5 21.8 49.2 49.5c0 0.7 0 1.3-0.1 2L1549.2 51.6z'/%3E%3Cg id='g'%3E%3Cuse href='%23s' transform='scale(0.12) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.2) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.25) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(0.3) rotate(-20)'/%3E%3Cuse href='%23s' transform='scale(0.4) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(0.5) rotate(20)'/%3E%3Cuse href='%23s' transform='scale(0.6) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.7) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.835) rotate(-40)'/%3E%3Cuse href='%23s' transform='scale(0.9) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(1.05) rotate(25)'/%3E%3Cuse href='%23s' transform='scale(1.2) rotate(8)'/%3E%3Cuse href='%23s' transform='scale(1.333) rotate(-60)'/%3E%3Cuse href='%23s' transform='scale(1.45) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(1.6) rotate(10)'/%3E%3C/g%3E%3C/defs%3E%3Cg transform='translate(1020 0)'%3E%3Cg transform='translate(0 360)'%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3Cg opacity='0.5'%3E%3Ccircle fill='url(%23a)' r='2000'/%3E%3Ccircle fill='url(%23a)' r='1800'/%3E%3Ccircle fill='url(%23a)' r='1700'/%3E%3Ccircle fill='url(%23a)' r='1651'/%3E%3Ccircle fill='url(%23a)' r='1450'/%3E%3Ccircle fill='url(%23a)' r='1250'/%3E%3Ccircle fill='url(%23a)' r='1175'/%3E%3Ccircle fill='url(%23a)' r='900'/%3E%3Ccircle fill='url(%23a)' r='750'/%3E%3Ccircle fill='url(%23a)' r='500'/%3E%3Ccircle fill='url(%23a)' r='380'/%3E%3Ccircle fill='url(%23a)' r='250'/%3E%3C/g%3E%3Cg transform='rotate(-241.2 0 0)'%3E%3Cuse href='%23g' transform='rotate(10)'/%3E%3Cuse href='%23g' transform='rotate(120)'/%3E%3Cuse href='%23g' transform='rotate(240)'/%3E%3C/g%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-          // backgroundAttachment: "fixed",
-          // backgroundSize: "cover",
-          // overflow: "auto",
-        }}
-      >
-        <Box // tri-colors card
-          sx={{
-            zIndex: "3",
-            position: "relative",
-            width: "100%",
-            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2371feed'/%3E%3Cstop offset='1' stop-color='%2371feed' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23abb2f9'/%3E%3Cstop offset='1' stop-color='%23abb2f9' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%231fa0f6'/%3E%3Cstop offset='1' stop-color='%231fa0f6' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23FFFFFF'/%3E%3Cstop offset='1' stop-color='%23FFFFFF' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2302DEC4'/%3E%3Cstop offset='1' stop-color='%2302DEC4' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%235865F2'/%3E%3Cstop offset='1' stop-color='%235865F2' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E")`,
-            backgroundAttachment: "fixed",
-            backgroundSize: "cover",
-            overflow: "auto",
-            borderRadius: "20px",
-          }}
-        >
-          {/* Filter Menu Button */}
-          <Box sx={{ position: "absolute", top: 20, left: 20, zIndex: 100 }}>
-            <FilterMenu
-              onKeywordFilter={setFilterKeyword}
-              onModalitiesFilter={setSelectedModalities}
-              filterKeyword={filterKeyword}
-              homeSelectedModalities={selectedModalities}
-            />
-          </Box>
-          <Box
-            sx={{
-              position: "absolute",
-              top: 80,
-              left: 20,
-              zIndex: 10,
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 3,
-              }}
-            >
-              <Typography
-                variant="h3"
-                sx={{
-                  color: Colors.purple,
-                  fontWeight: "bold",
-                }}
-              >
-                Discover and Explore
-              </Typography>
-              <Typography
-                variant="h6"
-                sx={{
-                  color: Colors.purple,
-                  width: "30%",
-                  display: { xs: "none", sm: "none", md: "none", lg: "block" },
-                }}
-              >
-                Dive into our interactive 3D graph to explore neuroimaging
-                databases. Visualize meaningful connections, filter by modality,
-                and access rich metadata instantly
-              </Typography>
-            </Box>
-
-            {/* Top Buttons - Show only on larger screens */}
-            <Box
-              sx={{
-                display: { xs: "none", md: "none", lg: "flex" },
-                flexDirection: "column",
-                width: { xs: "100%", sm: "50%", md: "30%", lg: "15%" },
-                maxWidth: "200px",
-                mb: 2,
-              }}
-            >
-              <Button
-                variant="outlined"
-                sx={{
-                  color: Colors.purple,
-                  borderColor: Colors.purple,
-                  transition: "all 0.3s ease",
-                  marginTop: 5,
-                  "&:hover": {
-                    transform: "scale(1.05)",
-                    borderColor: Colors.purple,
-                  },
-                }}
-              >
-                Start exploring
-                <ArrowForwardOutlinedIcon />
-              </Button>
-              <Button
-                variant="outlined"
-                sx={{
-                  color: Colors.purple,
-                  borderColor: Colors.purple,
-                  transition: "all 0.3s ease",
-                  marginTop: 4,
-                  "&:hover": {
-                    transform: "scale(1.05)",
-                    borderColor: Colors.purple,
-                  },
-                }}
-                onClick={() => navigate("/databases")}
-              >
-                View All Databases
-              </Button>
-            </Box>
-          </Box>
-
-          {/* 3d-graph */}
-          <Box sx={{ minHeight: "400px" }}>
-            {!registry ? (
-              <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-                <CircularProgress sx={{ color: Colors.primary.main }} />
-              </Box>
-            ) : filteredRegistry.length > 0 ? (
-              <NeuroJsonGraph
-                registry={filteredRegistry}
-                onNodeClick={handleNodeClick}
-              />
-            ) : (
-              <Box sx={{ textAlign: "center", mt: 25, ml: 30 }}>
-                <Typography variant="h6" color={Colors.darkPurple}>
-                  No matching nodes found
-                </Typography>
-              </Box>
-            )}
-          </Box>
-
-          {/* Bottom Buttons - Show only on smaller screens */}
-          <Box
-            sx={{
-              display: { xs: "flex", md: "flex", lg: "none" },
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: "20px 0",
-              width: "100%",
-            }}
-          >
-            <Button
-              variant="outlined"
-              sx={{
-                color: Colors.purple,
-                borderColor: Colors.purple,
-                transition: "all 0.3s ease",
-                marginBottom: 2,
-                width: "200px",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                  borderColor: Colors.purple,
-                },
-              }}
-            >
-              Start exploring
-              <ArrowForwardOutlinedIcon />
-            </Button>
-            <Button
-              variant="outlined"
-              sx={{
-                color: Colors.purple,
-                borderColor: Colors.purple,
-                transition: "all 0.3s ease",
-                width: "200px",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                  borderColor: Colors.purple,
-                },
-              }}
-              onClick={() => navigate("/databases")}
-            >
-              View All Databases
-            </Button>
-          </Box>
-
-          {/* view databases */}
-          {/* <Box sx={{ position: "absolute", bottom: 30, right: 50, zIndex: 10 }}>
-            <Button
-              variant="outlined"
-              sx={{
-                color: Colors.purple,
-                borderColor: Colors.purple,
-                transition: "all 0.3s ease",
-                marginTop: 4,
-                "&:hover": {
-                  transform: "scale(1.05)",
-                  borderColor: Colors.purple,
-                },
-              }}
-              onClick={() => navigate("/databases")}
-            >
-              View All Databases
-            </Button>
-          </Box> */}
-        </Box>
-      </Box>
-      {/* </Box> */}
+      <Section2
+        registry={registry}
+        filteredRegistry={filteredRegistry}
+        filterKeyword={filterKeyword}
+        selectedModalities={selectedModalities}
+        setFilterKeyword={setFilterKeyword}
+        setSelectedModalities={setSelectedModalities}
+        onNodeClick={handleNodeClick}
+      />
 
       {/* section 3 */}
       <Box
@@ -310,82 +90,8 @@ const Home: React.FC = () => {
           alignItems: "center",
         }}
       >
-        {/* <Box
-          sx={{
-            zIndex: "3",
-            position: "relative",
-            width: "100%",
-            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3CradialGradient id='a' gradientUnits='objectBoundingBox'%3E%3Cstop offset='0' stop-color='%23000000'/%3E%3Cstop offset='1' stop-color='%235865F2'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='750' x2='1550' y2='750'%3E%3Cstop offset='0' stop-color='%232c3379'/%3E%3Cstop offset='1' stop-color='%235865F2'/%3E%3C/linearGradient%3E%3Cpath id='s' fill='url(%23b)' d='M1549.2 51.6c-5.4 99.1-20.2 197.6-44.2 293.6c-24.1 96-57.4 189.4-99.3 278.6c-41.9 89.2-92.4 174.1-150.3 253.3c-58 79.2-123.4 152.6-195.1 219c-71.7 66.4-149.6 125.8-232.2 177.2c-82.7 51.4-170.1 94.7-260.7 129.1c-90.6 34.4-184.4 60-279.5 76.3C192.6 1495 96.1 1502 0 1500c96.1-2.1 191.8-13.3 285.4-33.6c93.6-20.2 185-49.5 272.5-87.2c87.6-37.7 171.3-83.8 249.6-137.3c78.4-53.5 151.5-114.5 217.9-181.7c66.5-67.2 126.4-140.7 178.6-218.9c52.3-78.3 96.9-161.4 133-247.9c36.1-86.5 63.8-176.2 82.6-267.6c18.8-91.4 28.6-184.4 29.6-277.4c0.3-27.6 23.2-48.7 50.8-48.4s49.5 21.8 49.2 49.5c0 0.7 0 1.3-0.1 2L1549.2 51.6z'/%3E%3Cg id='g'%3E%3Cuse href='%23s' transform='scale(0.12) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.2) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.25) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(0.3) rotate(-20)'/%3E%3Cuse href='%23s' transform='scale(0.4) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(0.5) rotate(20)'/%3E%3Cuse href='%23s' transform='scale(0.6) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.7) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.835) rotate(-40)'/%3E%3Cuse href='%23s' transform='scale(0.9) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(1.05) rotate(25)'/%3E%3Cuse href='%23s' transform='scale(1.2) rotate(8)'/%3E%3Cuse href='%23s' transform='scale(1.333) rotate(-60)'/%3E%3Cuse href='%23s' transform='scale(1.45) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(1.6) rotate(10)'/%3E%3C/g%3E%3C/defs%3E%3Cg transform='translate(100 0)'%3E%3Cg transform='translate(0 210)'%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3Cg opacity='0.5'%3E%3Ccircle fill='url(%23a)' r='2000'/%3E%3Ccircle fill='url(%23a)' r='1800'/%3E%3Ccircle fill='url(%23a)' r='1700'/%3E%3Ccircle fill='url(%23a)' r='1651'/%3E%3Ccircle fill='url(%23a)' r='1450'/%3E%3Ccircle fill='url(%23a)' r='1250'/%3E%3Ccircle fill='url(%23a)' r='1175'/%3E%3Ccircle fill='url(%23a)' r='900'/%3E%3Ccircle fill='url(%23a)' r='750'/%3E%3Ccircle fill='url(%23a)' r='500'/%3E%3Ccircle fill='url(%23a)' r='380'/%3E%3Ccircle fill='url(%23a)' r='250'/%3E%3C/g%3E%3Cg transform='rotate(-176.4 0 0)'%3E%3Cuse href='%23g' transform='rotate(10)'/%3E%3Cuse href='%23g' transform='rotate(120)'/%3E%3Cuse href='%23g' transform='rotate(240)'/%3E%3C/g%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-            backgroundAttachment: "fixed",
-            backgroundSize: "cover",
-            overflow: "auto",
-            borderRadius: "20px",
-            minHeight: "700px",
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            paddingRight: "3rem",
-          }}
-        > */}
         <img src="/workflow.png" alt="workflow" width="90%" height="auto"></img>
-        {/* </Box> */}
       </Box>
-
-      {/* Database Card Section */}
-      {/* <Box
-        sx={{
-          overflow: "hidden",
-          maxWidth: "35%",
-          // width: "100%",
-          zIndex: 3,
-          position: "absolute",
-          top: "5%",
-          left: "1%",
-          backgroundColor: "rgba(97, 109, 243, 0.4)",
-          // backgroundColor: "rgba(160, 165, 194, 0.4)",
-          backdropFilter: "blur(10px)",
-          // backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%235865f2' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%2302dec4' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%234c59d1' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%2347e3a9' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23424eb1' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%2377e789' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23394392' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23a3e768' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23303773' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%23d1e449' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23282c56' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23ffdd31' points='943 900 1210 900 971 687'/%3E%3C/svg%3E")`,
-          // backgroundAttachment: "local",
-          // backgroundSize: "cover",
-          padding: { xs: "1rem", sm: "1rem", md: "1.5rem" }, // Responsive padding
-          borderRadius: "8px",
-          flexShrink: 0,
-          // minHeight: "800px",
-        }}
-      >
-        
-        <Typography
-          variant="h3"
-          gutterBottom
-          sx={{ color: Colors.lightGray, fontWeight: "medium" }}
-        >
-          Discover NeuroJSON.io
-        </Typography>
-        <Typography variant="body1" sx={{ color: Colors.lightGray }}>
-          Efficiently manage and explore your CouchDB databases and datasets
-          with ease.
-        </Typography>
-
-       
-        <Box mt={4}>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: Colors.green,
-              color: Colors.darkPurple,
-              fontWeight: "Bold",
-              "&:hover": {
-                backgroundColor: Colors.darkPurple,
-                color: Colors.green,
-                boxShadow: `0px 0px 15px ${Colors.darkGreen}`,
-              },
-            }}
-            onClick={() => navigate("/databases")}
-          >
-            View Databases
-          </Button>
-        </Box>
-      </Box> */}
 
       <NodeInfoPanel
         open={panelOpen}
