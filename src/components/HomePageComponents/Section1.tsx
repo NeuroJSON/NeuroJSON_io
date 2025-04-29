ce394c1f59d3d47f5b48b259b1748323d68c7332
elainefan331 <elainefan331@gmail.com>
2025-04-29 12:15:47 -0400

    landing page section1 img path testing

--- src/components/HomePageComponents/Section1.tsx
+++ src/components/HomePageComponents/Section1.tsx
@@ -48,7 +48,8 @@ const Section1: React.FC = () => {
             }}
           >
             <img
-              src="./3Dgraph.png"
+              // src="./3Dgraph.png"
+              src={`${process.env.PUBLIC_URL}/3Dgraph.png`}
               alt="3d_graph"
               width="30%"
               height="auto"
