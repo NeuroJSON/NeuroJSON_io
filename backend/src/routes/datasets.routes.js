99cd3be103d461ce34ca34fed2bab8c4af34db5d
elainefan331 <elainefan331@gmail.com>
2025-11-10 15:29:04 -0500

    add datasets routes

---
+++ backend/src/routes/datasets.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+  getDatasetDetail,
+  getDatasetMeta,
+} = require("../controllers/couchdb.controller");
+
+const router = express.Router();
+
+// Dataset routes
+router.get("/:dbName/:datasetId", getDatasetDetail);
+router.get("/:dbName/:datasetId/meta", getDatasetMeta);
+
+module.exports = router;
