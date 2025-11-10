6e5aa8cb6f01d068fbd58ca7aa1b999b2e7b8191
elainefan331 <elainefan331@gmail.com>
2025-11-10 15:26:57 -0500

    add dbs routes

---
+++ backend/src/routes/dbs.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  getDbList,
+  getDbStats,
+  getDbInfo,
+  getDbDatasets,
+  searchAllDatabases,
+  searchDatabase,
+} = require("../controllers/couchdb.controller");
+
+const router = express.Router();
+
+// Database list and stats
+router.get("/", getDbList);
+router.get("/stats", getDbStats);
+
+// cross dbs search
+router.post("/search", searchAllDatabases);
+
+// Specific database routes
+router.get("/:dbName", getDbInfo);
+router.get("/:dbName/datasets", getDbDatasets);
+router.post("/:dbName/search", searchDatabase); // search within one db
+
+module.exports = router;
